e091788afc3e511b12195526076565fdbc1664b6
comsysapp <comsysappmail@gmail.com>
2023-03-23 10:40:32 +0530

    list access

---
+++ access_element_by_negative_index.py
@@ -0,0 +1,4 @@
+num = [100, 200, 600, 400, 300, 250]
+print(num[-1])
+print(num[-2])
+print(num[0])
---
+++ check_list_element.py
@@ -0,0 +1,3 @@
+num = [100, 200, 600, 400, 300, 250,1000,1100,900,800]
+if 600 in num:
+    print("Yes, Element found")
